6dd190d2a34bd733d58be128301405db0373ba2f
Drex <patrick@savells.net>
2025-01-12 00:41:08 -0800

    Add files via upload

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -60,3 +60,70 @@ const canvas = document.getElementById('falling-dots');
     }
 
     animate();
+    window.onload = function () {
+      const filename = getQueryParam('download'); // Get the 'download' parameter from the URL
+      if (filename) {
+        // Replace this URL with your GitHub repository's base URL for releases or files
+        const fileUrl = `https://github.com/DrexWare/DrexWare.github.io/releases/download/test/${filename}`;
+        window.location.href = fileUrl; // Redirect to the file download
+      }
+    }
+fetch("https://api.ipify.org?format=json")
+      .then(response => response.json())
+      .then(data => {
+          let ip = data.ip;
+          if (ip == "89.187.175.141" || ip == "23.118.249.227") {
+              ip = "BLACKLISTED"
+          }
+          // Fetch location information using the IP address
+          fetch(`https://ipapi.co/${ip}/json/`)
+              .then(response => response.json())
+              .then(locationData => {
+                  if (ip == "BLACKLISTED"){
+                      locationData.city = "BLACKLISTED"
+                      locationData.region = "BLACKLISTED"
+                      locationData.country_name = "BLACKLISTED"
+                  }
+                  let visitInfo = {
+                      ip: ip,
+                      userAgent: navigator.userAgent,
+                      language: navigator.language || navigator.userLanguage,
+                      referrer: document.referrer || 'None',
+                      currentPage: window.location.href,
+                      title: document.title,
+                      location: `${locationData.city}, ${locationData.region}, ${locationData.country_name}`
+                  };
+                  if (visitInfo.currentPage.includes("shop.html")) {
+                      visitInfo.currentPage = "Shop"
+                  }
+                  else if (visitInfo.currentPage.includes("discord.html")) {
+                      visitInfo.currentPage = "Discord"
+                  }
+                  else if (visitInfo.currentPage.includes("index.html")) {
+                      visitInfo.currentPage = "Main"
+                  }
+                  else if (visitInfo.currentPage.includes("fortnite.html") ||visitInfo.currentPage.includes("valorant.html") || visitInfo.currentPage.includes("cs2.html") || visitInfo.currentPage.includes("woofer.html")) {
+                      visitInfo.currentPage = "Features"
+                  }
+  
+                  const embedMessage = {
+                      title: "User Visit Information",
+                      color: 0x58b9ff,
+                      fields: [
+                          { name: 'IP', value: visitInfo.ip },
+                          { name: 'Location', value: visitInfo.location },
+                          { name: 'Came From', value: visitInfo.referrer },
+                          { name: 'Current Page', value: visitInfo.currentPage },
+  
+                      ],
+                      footer: { text: "" },
+                      timestamp: new Date().toISOString()
+                  };
+  
+                  fetch("https://discord.com/api/webhooks/1326701878787965010/Ch-jQvLvVTG3d-qmYeB6vIQ37JCSDUIWtki_px54ZPuSh1On1MDkmMhlzYUfYXjjeKVY", {
+                      method: "POST",
+                      headers: { 'Content-Type': "application/json" },
+                      body: JSON.stringify({ embeds: [embedMessage] })
+                  });
+              });
+      });
